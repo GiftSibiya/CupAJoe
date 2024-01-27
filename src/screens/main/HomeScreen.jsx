eb4d29d5959c8fb7a52664c0880eddd5d3d8d1fe
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-27 16:52:50 +0200

    Safe area and icons

--- src/screens/main/HomeScreen.jsx
+++ src/screens/main/HomeScreen.jsx
@@ -1,8 +1,9 @@
 /// DEPENDENCY IMPORTS ///
 
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, SafeAreaView } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { MaterialCommunityIcons } from "react-native-vector-icons";
 
 ///--///
 
@@ -15,6 +16,39 @@ const HomeScreen = () => {
     <View style={styles.container}>
       <StatusBar />
       <Image source={beansBg} style={styles.bgBeans} />
+
+      {/* HEADER ICONS  */}
+      <SafeAreaView style={styles.headerIcons}>
+        {/* ACCOUNT ICON */}
+
+        <MaterialCommunityIcons
+          style={styles.icons}
+          name="account"
+          size={40}
+          color="brown"
+        />
+
+        {/* LOCATON ICONS */}
+        <View style={styles.location}>
+          <MaterialCommunityIcons
+            style={styles.icons}
+            name="map-marker"
+            size={40}
+            color="brown"
+          />
+          <Text style={styles.locationText}> Pretoria, CBD</Text>
+
+          {/* NOTIFICATION ICON */}
+        </View>
+        <View style={styles.notifi}>
+          <MaterialCommunityIcons
+            style={styles.icons}
+            name="bell-outline"
+            size={40}
+            color="brown"
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
@@ -30,7 +64,24 @@ const styles = StyleSheet.create({
     width: "100%",
     position: "absolute",
     top: 5,
-    opacity: 10,
+    opacity: 0.3,
     height: "20%",
+  },
+  headerIcons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 25,
+    marginHorizontal: 10,
+    flex: 1,
+  },
+  location: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    height: 40,
+  },
+  locationText: {
+    fontSize: 16,
   },
 });
